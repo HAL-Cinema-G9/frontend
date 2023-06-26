7dab8a32567d9b45ac9900d6b539aa68474a414b
tyem0802 <pssptyen@gmail.com>
2023-06-26 15:52:51 +0900

    Revert "html/基礎CSS作成"
    This reverts commit bf9104204b14a0365606f869746ce8010d7fb56a.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,5 @@
-import { css } from '@emotion/react';
-
-const styles = {
-  container: css`
-    height: 100px;
-    width: 100%;
-
-    display: flex;
-
-    background-color: gray;
-  `,
-  menu: css`
-    background-color: red;
-  `,
-  logo: css`
-    background-color: blue;
-    margin: 0 auto;
-  `,
-  account: css`
-    background-color: red;
-  `,
-  search: css`
-    background-color: blue;
-  `,
-};
-
 const Navbar = () => {
-  return (
-    <div css={styles.container}>
-      <div css={styles.menu}>burger</div>
-      <div css={styles.logo}>HALCINEMA</div>
-      <div css={styles.account}>account</div>
-      <div css={styles.search}>search</div>
-    </div>
-  );
+  return <div>Navbar</div>;
 };
 
 export default Navbar;
