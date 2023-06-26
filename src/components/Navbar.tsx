bf9104204b14a0365606f869746ce8010d7fb56a
tyem0802 <pssptyen@gmail.com>
2023-06-26 15:42:58 +0900

    html/基礎CSS作成

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,38 @@
+import { css } from '@emotion/react';
+
+const styles = {
+  container: css`
+    height: 100px;
+    width: 100%;
+
+    display: flex;
+
+    background-color: gray;
+  `,
+  menu: css`
+    background-color: red;
+  `,
+  logo: css`
+    background-color: blue;
+    margin: 0 auto;
+  `,
+  account: css`
+    background-color: red;
+  `,
+  search: css`
+    background-color: blue;
+  `,
+};
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div css={styles.container}>
+      <div css={styles.menu}>burger</div>
+      <div css={styles.logo}>HALCINEMA</div>
+      <div css={styles.account}>account</div>
+      <div css={styles.search}>search</div>
+    </div>
+  );
 };
 
 export default Navbar;
