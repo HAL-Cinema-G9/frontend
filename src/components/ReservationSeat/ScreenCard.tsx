c66e2ada086c305326fd7d0fe7df611ec122236d
yoshi-non <himekawa.yoshino44@gmail.com>
2023-07-25 07:18:18 +0900

    add 座席表の表示

--- src/components/ReservationSeat/ScreenCard.tsx
+++ src/components/ReservationSeat/ScreenCard.tsx
@@ -8,11 +8,45 @@ import { css } from '@emotion/react';
 const styles = {
   container: css`
     padding: 100px 0;
-    background-color: #524160;
+    background-color: #f5f5f5;
     width: 100%;
+  `,
+  screenWrapper: css`
     display: flex;
-    flex-direction: column;
-    gap: 100px;
+    justify-content: center;
+    margin: 0 auto;
+    width: 100%;
+    max-width: 1200px;
+  `,
+
+  seatsWrapper: css`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  `,
+
+  seatGroupWrapper: css`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  `,
+
+  seatCardWrapper: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+  seat: css`
+    border: 1px solid #ccc;
+    margin: 5px;
+    width: 40px;
+    height: 40px;
+    background-color: gray;
+  `,
+  seatColumn: css`
+    width: 40px;
+    height: 40px;
+    text-align: center;
   `,
 };
 
@@ -26,28 +60,47 @@ type Props = {
 
 const ScreenCard = ({ props }: Props) => {
   const { schedule, seats, reservations } = props;
+
+  // 20件ごとに座席をグループ化する関数
+  const groupSeats = (seats: Seat[], groupSize: number) => {
+    const groups = [];
+    for (let i = 0; i < seats.length; i += groupSize) {
+      groups.push(seats.slice(i, i + groupSize));
+    }
+    return groups;
+  };
+
+  // 20件ごとの座席グループを作成
+  const seatGroups = groupSeats(seats, 20);
   return (
-    <div>
+    <div css={styles.container}>
       <h2>{schedule.movie.title}</h2>
       <div>座席一覧</div>
-      {seats.map((seat) => (
-        <div key={seat.id}>
-          <div>
-            column:{seat.column},row:{seat.row}
-          </div>
-          <div>
-            {/* reservationsの中にseat.idと同じモノが合ったらfalse */}
-            {reservations.some(
-              (reservation) =>
-                reservation.seat_id === seat.id
-            ) ? (
-              <div>予約済み</div>
-            ) : (
-              <div>空席</div>
-            )}
-          </div>
+      <div css={styles.screenWrapper}>
+        <div css={styles.seatsWrapper}>
+          {seatGroups.map((group, index) => (
+            <div key={index} css={styles.seatGroupWrapper}>
+              {group.map((seat) => (
+                <div
+                  key={seat.id}
+                  css={styles.seatCardWrapper}
+                >
+                  <span css={styles.seat}>
+                    {seat.column}
+                    {seat.row}
+                  </span>
+                  {/* seat.rowが4か16のときは隣にcolumnを表示 */}
+                  {seat.row === 4 || seat.row === 16 ? (
+                    <span css={styles.seatColumn}>
+                      {seat.column}
+                    </span>
+                  ) : null}
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
