ddf676e7ca03e70297e9a7c0d0e9ad88699e7e7e
yoshi-non <himekawa.yoshino44@gmail.com>
2023-08-03 08:11:20 +0900

    add 基礎コンポーネントの作成

---
+++ src/components/MyPage/MyPageCard.tsx
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+const styles = {
+  container: css``,
+};
+const MyPageCard = () => {
+  return <div css={styles.container}>MyPageCard</div>;
+};
+
+export default MyPageCard;
---
+++ src/pages/mypage.tsx
@@ -0,0 +1,15 @@
+import Footer from '@/components/Footer';
+import MyPageCard from '@/components/MyPage/MyPageCard';
+import Navbar from '@/components/Navbar';
+
+const MyPage = () => {
+  return (
+    <main>
+      <Navbar />
+      <MyPageCard />
+      <Footer />
+    </main>
+  );
+};
+
+export default MyPage;
