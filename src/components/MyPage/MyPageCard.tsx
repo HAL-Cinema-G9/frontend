60a7114f4e570c143bf6fcb100d3b88415240c8f
yoshi-non <himekawa.yoshino44@gmail.com>
2023-08-05 12:20:23 +0900

    add 予約リスト

--- src/components/MyPage/MyPageCard.tsx
+++ src/components/MyPage/MyPageCard.tsx
@@ -2,7 +2,13 @@ import { css } from '@emotion/react';
 import { useState } from 'react';
 import Logout from '../Signin/Button/LogoutButton';
 import { useSession } from 'next-auth/react';
-import { ReservationType } from '@/types/apiTypes';
+import {
+  ReservationType,
+  Schedule,
+} from '@/types/apiTypes';
+import { startMovieTime } from '@/utils/startMovieTime';
+import { addCommas } from '@/utils/addCommas';
+import { endMovieTime } from '@/utils/endMovieTime';
 
 const styles = {
   container: css`
@@ -40,6 +46,22 @@ const styles = {
       }
     }
   `,
+  reverseWrapper: css`
+    flex-direction: column-reverse;
+  `,
+  reservationWrapper: css`
+    margin: 10px 0;
+    padding: 10px 0 30px 0;
+    border-bottom: 1px solid #616161;
+    & > h4 {
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
+    & > p {
+      font-size: 1.2rem;
+      color: #919191;
+    }
+  `,
 };
 
 type Props = {
@@ -51,7 +73,26 @@ const MyPageCard = ({ myReservations }: Props) => {
     useState<string>('account');
   const { data: session, status } = useSession();
 
-  console.log(myReservations);
+  const getFormattedDate = (schedule: Schedule) => {
+    const scheduleDate = new Date(schedule.date);
+    const year = scheduleDate.getFullYear();
+    const month = (scheduleDate.getMonth() + 1)
+      .toString()
+      .padStart(2, '0');
+    const date = scheduleDate
+      .getDate()
+      .toString()
+      .padStart(2, '0');
+
+    const formattedDate = `${year}年${month}月${date}日（水）${startMovieTime(
+      scheduleDate
+    )}~${endMovieTime(
+      scheduleDate,
+      schedule.movie.duration
+    )}`;
+    return formattedDate;
+  };
+
   return (
     <div css={styles.container}>
       <div css={styles.selectNavbar}>
@@ -94,6 +135,43 @@ const MyPageCard = ({ myReservations }: Props) => {
           <div>
             <Logout />
           </div>
+        </div>
+      )}
+      {showSelectNav === 'reservationList' && (
+        <div
+          css={[
+            styles.selectDetailWrapper,
+            styles.reverseWrapper,
+          ]}
+        >
+          {myReservations.map((reservation) => (
+            <div
+              key={reservation.id}
+              css={styles.reservationWrapper}
+            >
+              <h4>{reservation.schedule.movie.title}</h4>
+              <p>
+                座席：{reservation.seat.column}
+                {reservation.seat.row}
+              </p>
+              <p>
+                上映時刻：
+                {getFormattedDate(reservation.schedule)}
+              </p>
+              <p>
+                スクリーン：
+                {reservation.schedule.screen.name}
+              </p>
+              <p>チケット：{reservation.ticket.name}</p>
+              <p>
+                金額：
+                {addCommas(
+                  Math.floor(reservation.ticket.price)
+                )}
+                円
+              </p>
+            </div>
+          ))}
         </div>
       )}
     </div>
